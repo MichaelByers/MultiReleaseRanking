bf4cab6b215b829d90b8c4ff78835d34482ae489
Michael Byers <michael.byers@mckesson.com>
2013-05-31 11:50:25 -0600

    Added Release multiobjectpicker

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
+	margin: 15,
+	cls: 'release-ranking',
 
     launch: function() {
+		var dataContext = this.context.getDataContext();
+		dataContext.projectScopeDown = false;
+		
         this.add({
 			xtype: 'component',
 			cls: 'title',
 			html: 'Multi Release Ranking',
 			padding: '0, 0, 10, 0'
-        });
+        },
+		{
+			xtype: 'rallymultiobjectpicker',
+			modelType: 'release',
+			fieldLable: 'Select a release',
+			listeners: {
+				blur: function(picker) {
+				}
+			},
+			storeConfig: {
+				filters: [
+				{
+					property: 'State',
+					operator: '!=',
+					value: 'Accepted'
+				}
+				],
+				context: dataContext
+			}	
+		});
+		
+		
     }
 });
