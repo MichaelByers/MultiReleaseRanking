fb764b15add36c0391b7b0c08684aa610374ce23
Michael Byers <michael.byers@mckesson.com>
2013-05-31 14:42:14 -0600

    added story and defect query, then sort

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ Ext.define('CustomApp', {
 			modelType: 'release',
 			fieldLable: 'Select a release',
 			listeners: {
-				blur: this._createGrid,
+				blur: this._runQuery,
 				scope: this
 			},
 			storeConfig: {
@@ -34,40 +34,114 @@ Ext.define('CustomApp', {
 			}	
 		});
 
-		filter = new Rally.data.QueryFilter({
-			property: 'StartDate',
-			operator: '>=',
-			value: this.qDate
-		});
-		var newFilter = new Rally.data.QueryFilter({
-			property: 'EndDate',
-			operator: '<=',
-			value: Rally.util.DateTime.toIsoString(Rally.util.DateTime.add(new Date(), 'day', 21))
-		});
-		filter = filter.and(newFilter);
-
-    	var myContext = this.context.getDataContext();
-    	myContext.projectScopeDown = false;
-
-   		Ext.create('Rally.data.WsapiDataStore', {
-            model: 'Iteration',
-            context: myContext,
-            autoLoad: true,
-    		filters: filter,
-        	fetch: 'Name,StartDate,EndDate',
-		    sorters: [
-		              {
-		                  property: 'StartDate',
-		                  direction: 'ASC'
-		              }
-		    ],
-            listeners: {
-                load: this._onIterationsLoaded,
-                scope: this
-            }
-        });
-
     },
+	
+    _runQuery: function(picker) {
+    	var myContext = this.context.getDataContext();
+    	myContext.projectScopeDown = true;
+    	var filter = null;
+    	var pRelease = picker.getValue();
+    	
+    	if((pRelease != null) && (pRelease.length > 0)) {
+        	for(var i=0; i<pRelease.length; i++) {
+        		var release = pRelease[i].data.Name;
+        		var newFilter = new Rally.data.QueryFilter({
+    				property: 'Release.Name',
+    				operator: '=',
+    				value: release
+    			});
+        		if(filter == null) {
+        			filter = newFilter;
+        		}
+        		else {
+        			filter = filter.or(newFilter);
+        		}
+        	}
+       		Ext.create('Rally.data.WsapiDataStore', {
+                model: 'UserStory',
+                context: myContext,
+                autoLoad: true,
+        		filters: filter,
+            	fetch: 'Name,FormattedID,ScheduleState,PlanEstimate,KanbanState,Iteration,Release,Project,Ready,Rank,Blocked',
+    		    sorters: [
+    		              {
+    		                  property: 'Rank'
+    		              }
+    		    ],
+                listeners: {
+                    load: function(store, data) {
+                    	this._onStoriesLoaded(store, data, picker);
+                    },
+                    scope: this
+                }
+            });    	    		
+    	}
+    },
+    
+	_onStoriesLoaded:  function(sStore, sData, picker) {
+    	var myContext = this.context.getDataContext();
+    	myContext.projectScopeDown = true;
+    	var filter = null;
+    	var pRelease = picker.getValue();
+    	
+    	if((pRelease != null) && (pRelease.length > 0)) {
+        	for(var i=0; i<pRelease.length; i++) {
+        		var release = pRelease[i].data.Name;
+        		var newFilter = new Rally.data.QueryFilter({
+    				property: 'Release.Name',
+    				operator: '=',
+    				value: release
+    			});
+        		if(filter == null) {
+        			filter = newFilter;
+        		}
+        		else {
+        			filter = filter.or(newFilter);
+        		}
+        	}
+       		Ext.create('Rally.data.WsapiDataStore', {
+                model: 'Defect',
+                context: myContext,
+                autoLoad: true,
+        		filters: filter,
+            	fetch: 'Name,FormattedID,ScheduleState,PlanEstimate,c_KanbanState,Iteration,Release,Project,Ready,Rank,Blocked',
+    		    sorters: [
+    		              {
+    		                  property: 'Rank'
+    		              }
+    		    ],
+                listeners: {
+                    load: function(dStore, dData) {
+                    	this._onDefectsLoaded(dStore, dData, sData);
+                    },
+                    scope: this
+                }
+            });    	    		
+    	}
+		
+	},
+	
+	_onDefectsLoaded: function(store, dData, sData) {
+		// combine stories and defect data
+		var myData = sData.concat(dData);
+		// extract data
+		var d = new Array();
+		for(var i=0; i<myData.length; i++) {
+			d[i] = myData[i].data;
+		}
+		// sort
+		this._sortByKey(d, 'Rank');
+		for(var i=0; i<d.length; i++) {
+			console.log(d[i].FormattedID+ " " + d[i].Rank);
+		}
+	},
+
+	_sortByKey: function (array, key) {
+	    return array.sort(function(a, b) {
+	        var x = a[key]; var y = b[key];
+	        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+	    });
+	},
 	
 	_createGrid:  function(picker) {
 		this.add({
