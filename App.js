7d0abd2f74381ca0bfd06262c277882bc3b8b238
Michael Byers <michael.byers@mckesson.com>
2013-06-01 10:40:50 -0600

    Working inline editing and drag-n-drop ranking on multiple record types

--- App.js
+++ App.js
@@ -1,243 +1,233 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-	margin: 15,
-	cls: 'release-ranking',
+    margin: 15,
+    cls: 'release-ranking',
 
-    launch: function() {
-		var dataContext = this.context.getDataContext();
-		dataContext.projectScopeDown = false;
-		
+    launch: function () {
+        var dataContext = this.context.getDataContext();
+        dataContext.projectScopeDown = false;
+
         this.add({
-			xtype: 'component',
-			cls: 'title',
-			html: 'Multi Release Ranking',
-			padding: '0, 0, 10, 0'
-        },
-		{
-			xtype: 'rallymultiobjectpicker',
-			modelType: 'release',
-			fieldLable: 'Select a release',
-			listeners: {
-				blur: this._runQuery,
-				scope: this
-			},
-			storeConfig: {
-				filters: [
-				{
-					property: 'State',
-					operator: '!=',
-					value: 'Accepted'
-				}
-				],
-				context: dataContext
-			}	
-		});
-
-    },
-	
-    _runQuery: function(picker) {
-    	var myContext = this.context.getDataContext();
-    	myContext.projectScopeDown = true;
-    	var filter = null;
-    	var pRelease = picker.getValue();
-    	
-    	if((pRelease != null) && (pRelease.length > 0)) {
-        	for(var i=0; i<pRelease.length; i++) {
-        		var release = pRelease[i].data.Name;
-        		var newFilter = new Rally.data.QueryFilter({
-    				property: 'Release.Name',
-    				operator: '=',
-    				value: release
-    			});
-        		if(filter == null) {
-        			filter = newFilter;
-        		}
-        		else {
-        			filter = filter.or(newFilter);
-        		}
-        	}
-       		Ext.create('Rally.data.WsapiDataStore', {
-                model: 'UserStory',
-                context: myContext,
-                autoLoad: true,
-        		filters: filter,
-            	fetch: 'Name,FormattedID,ScheduleState,PlanEstimate,c_KanbanState,Iteration,Release,Project,Ready,Rank,Blocked',
-    		    sorters: [
-    		              {
-    		                  property: 'Rank'
-    		              }
-    		    ],
+                xtype: 'component',
+                cls: 'title',
+                html: 'Multi Release Ranking',
+                padding: '0, 0, 10, 0'
+            },
+            {
+                xtype: 'rallymultiobjectpicker',
+                modelType: 'release',
+                fieldLable: 'Select a release',
                 listeners: {
-                    load: function(store, data) {
-                    	this._onStoriesLoaded(store, data, picker);
-                    },
+                    blur: this._runQuery,
                     scope: this
-                }
-            });    	    		
-    	}
-    },
-    
-	_onStoriesLoaded:  function(sStore, sData, picker) {
-    	var myContext = this.context.getDataContext();
-    	myContext.projectScopeDown = true;
-    	var filter = null;
-    	var pRelease = picker.getValue();
-    	
-    	if((pRelease != null) && (pRelease.length > 0)) {
-        	for(var i=0; i<pRelease.length; i++) {
-        		var release = pRelease[i].data.Name;
-        		var newFilter = new Rally.data.QueryFilter({
-    				property: 'Release.Name',
-    				operator: '=',
-    				value: release
-    			});
-        		if(filter == null) {
-        			filter = newFilter;
-        		}
-        		else {
-        			filter = filter.or(newFilter);
-        		}
-        	}
-       		Ext.create('Rally.data.WsapiDataStore', {
-                model: 'Defect',
-                context: myContext,
-                autoLoad: true,
-        		filters: filter,
-            	fetch: 'Name,FormattedID,ScheduleState,PlanEstimate,c_KanbanState,Iteration,Release,Project,Ready,Rank,Blocked',
-    		    sorters: [
-    		              {
-    		                  property: 'Rank'
-    		              }
-    		    ],
-                listeners: {
-                    load: function(dStore, dData) {
-                    	this._onDefectsLoaded(dStore, dData, sData);
-                    },
-                    scope: this
-                }
-            });    	    		
-    	}
-		
-	},
-	
-	_onDefectsLoaded: function(store, dData, sData) {
-		// combine stories and defect data
-		var myData = sData.concat(dData);
-		
-		// extract data
-		var d = new Array();
-		for(var i=0; i<myData.length; i++) {
-			d[i] = myData[i].data;
-		}
-		// sort
-		this._sortByKey(d, 'Rank');
-		
-		Rally.data.ModelFactory.getModel({
-			type: 'UserStory',
-			success: function(userStoryModel) {
-				var dataStore =  Ext.create('Rally.data.custom.Store', {
-		        	model: userStoryModel,
-					data: d,
-					sorters: [
-						{
-							property: 'Rank'
-						}
-					]
-		    	});
-				this._createGrid(dataStore);
-			},
-			scope: this
-		});
-	},
-
-	_sortByKey: function (array, key) {
-	    return array.sort(function(a, b) {
-	        var x = a[key]; var y = b[key];
-	        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-	    });
-	},
-	
-	_createGrid:  function(dataStore) {
-	    var cellEditing = Ext.create('Ext.grid.plugin.CellEditing', {
-	        clicksToEdit: 1
-	    });
-
-//	    var comboParameter = Ext.create('ComboBoxParameter');
-	    
-		this.add({
-			xtype: 'rallygrid',
-			store: dataStore,
-			disableColumnMenus: false,
-			autoAddAllModelFieldsAsColumns: true,
-			id: 'grid',
-/*		    columns: [
-		              {xtype: 'rownumberer'},
-		              {
-		                  header: 'Manual Rank Above',
-		                  xtype: 'numbercolumn',
-		                  width: 60,
-		                  sortable: false,
-		                  editor: {
-		                      xtype: 'numberfield',
-		                      allowBlank: true
-		                  }
-		              },{
-		                  header: 'ID',
-		                  dataIndex: 'FormattedID',
-		                  width: 60,
-		                  sortable: false
-		              },{
-		                  header: 'Kanban',
-		                  dataIndex: 'c_KanbanState',
-		                  width: 100,
-		                  sortable: true,
-		                  editor: {
-		                      xtype: 'textfield',
-		                      allowBlank: true
-		                  }
-		              }
-		    ],
-*/
-			columnCfgs: [
-				{xtype: 'rownumberer'},
-				{text: 'Manual Rank Above', xtype:'numbercolumn', width: 60, editor: {
-                    	xtype: 'numberfield',
-                    	allowBlank: true }
                 },
-				{text: 'ID', dataIndex: 'FormattedID', width: 60},
-				{text: 'Name', dataIndex: 'Name', width: 500},
-				{text: 'Kanban', dataIndex: 'c_KanbanState', width: 100, editor: {
-	                xtype: 'combobox',
-	                typeAhead: true,
-	                triggerAction: 'all',
-	                selectOnTab: true,
-	                store: [
-	                    ['Prioritized','Prioritized'],
-	                    ['Analysis','Analysis'],
-	                    ['Development','Development'],
-	                    ['Peer Review','Peer Review'],
-	                    ['QA','QA'],
-	                    ['Completed', 'Completed'],
-	                    ['Accepted', 'Accepted']
-	                ],
-	                lazyRender: true,
-	                listClass: 'x-combo-list-small'
-	            } },
-				{text: 'Release', dataIndex: 'Release', width: 150},
-				{text: 'Ready', dataIndex: 'Ready', width: 50}
-			],
-			enableRanking: true,
-			plugins: [cellEditing],
+                storeConfig: {
+                    filters: [
+                        {
+                            property: 'State',
+                            operator: '!=',
+                            value: 'Accepted'
+                        }
+                    ],
+                    context: dataContext
+                }
+            });
+
+    },
+
+    _runQuery: function (picker) {
+        var filter = null;
+        var pRelease = picker.getValue();
+
+        if ((pRelease !== null) && (pRelease.length > 0)) {
+            for (var i = 0; i < pRelease.length; i++) {
+                var release = pRelease[i].data.Name;
+                var newFilter = new Rally.data.QueryFilter({
+                    property: 'Release.Name',
+                    operator: '=',
+                    value: release
+                });
+                if (filter === null) {
+                    filter = newFilter;
+                }
+                else {
+                    filter = filter.or(newFilter);
+                }
+            }
+            this.filter = filter;
+			this._createStoreForGrid();
+        }
+    },
+
+    _createStoryStore: function () {
+        Ext.create('Rally.data.WsapiDataStore', {
+            model: 'UserStory',
+            autoLoad: true,
+            filters: this.filter,
+            fetch: 'Name,FormattedID,ScheduleState,PlanEstimate,c_KanbanState,Iteration,Release,Project,Ready,Rank,Blocked',
+            sorters: [
+                {
+                    property: 'Rank'
+                }
+            ],
+            listeners: {
+                load: function (store, data) {
+                    this._onStoriesLoaded(store, data);
+                },
+                scope: this
+            }
+        });
+    },
+
+    _onStoriesLoaded: function (sStore, sData) {
+        Ext.create('Rally.data.WsapiDataStore', {
+            model: 'Defect',
+            autoLoad: true,
+            filters: this.filter,
+            fetch: 'Name,FormattedID,ScheduleState,PlanEstimate,c_KanbanState,Iteration,Release,Project,Ready,Rank,Blocked',
+            sorters: [
+                {
+                    property: 'Rank'
+                }
+            ],
+            listeners: {
+                load: function (dStore, dData) {
+                    this._onDefectsLoaded(dStore, dData, sData);
+                },
+                scope: this
+            }
+        });
+    },
+
+    _onDefectsLoaded: function (store, dData, sData) {
+        // combine stories and defect data
+        var myData = sData.concat(dData);
+
+        // extract data
+        var d = new Array();
+        for (var i = 0; i < myData.length; i++) {
+            d[i] = myData[i].data;
+        }
+        // sort
+        this._sortByKey(d, 'Rank');
+		var grid = this.down('rallygrid');
+		if (grid) {
+			grid.getStore().loadData(d);
+		}
+    },
+
+    _sortByKey: function (array, key) {
+        return array.sort(function (a, b) {
+            var x = a[key];
+            var y = b[key];
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        });
+    },
+	
+	_createStoreForGrid: function() {
+		this.previousState = 'commit';
+        Rally.data.ModelFactory.getModel({
+            type: 'UserStory',
+            success: function (userStoryModel) {
+                var dataStore = Ext.create('Rally.data.WsapiDataStore', {
+                    model: userStoryModel,
+                    filters: [
+					{
+						property: 'Name',
+						value: 'Does Not Exist Please Work'
+					}
+					],
+                    sorters: [
+                        {
+                            property: 'Rank'
+                        }
+                    ],
+					listeners: {
+						update: function(store, record, operation, modifiedFieldNames) {
+							if (operation === this.previousState) {
+								this._createStoryStore();
+							}
+							this.previousState = operation;
+						},
+						create: this._createStoryStore,
+						scope: this
+					}
+				});
+				dataStore.on('load', this._createStoryStore, this, {single: true});
+				this._createGrid(dataStore);
+            },
+            scope: this
+        });
+	},
+
+    _createGrid: function (dataStore) {
+		//TODO: cleanup, move to its own class
+        this._overrideProxyWriter(dataStore);
+
+		var grid = this.down('rallygrid');
+		if (grid) {
+			grid.destroy();
+		}
+        this.add({
+            xtype: 'rallygrid',
+            store: dataStore,
+            disableColumnMenus: false,
+            autoAddAllModelFieldsAsColumns: true,
+            columnCfgs: [	
+                {xtype: 'rownumberer'},
+                {text: 'Manual Rank Above', xtype: 'numbercolumn', width: 60},
+                'FormattedID',
+               	'Name',
+				'c_KanbanState',
+                'Release',
+				'Ready'
+            ],
+            enableRanking: true,
 			viewConfig: {
 				stripeRows: true,
-		        //Return CSS class to apply to rows depending upon data values
-		        getRowClass: function(record, index) {
-		            var c = record.get('Ready');
-	            	return "yellow-back";
-		        }
-		    }
-		});
-	}
+				//Return CSS class to apply to rows depending upon data values
+				getRowClass: function(record, index) {
+					var c = record.get('Ready');
+				    return "yellow-back";
+				}
+			}
+        });   
+		this.down('rallygrid').getStore().load();
+	},
 
+    _overrideProxyWriter: function(dataStore) {
+       Ext.override(dataStore.getProxy().writer, {
+            writeRecords: function (request, data) {
+                request.url = request.url.replace("hierarchicalrequirement", request.records[0].get('_type'));
+                request.url = request.url.replace("HierarchicalRequirement", request.records[0].get('_type'));
+                var root = request.records[0].get('_type');
+
+                if (this.allowSingle && data.length == 1) {
+                    // convert to single object format
+                    data = data[0];
+                }
+
+                if (this.encode) {
+                    if (root) {
+                        // sending as a param, need to encode
+                        request.params[root] = Ext.encode(data);
+                    } else {
+                        //<debug>
+                        Ext.Error.raise('Must specify a root when using encode');
+                        //</debug>
+                    }
+                } else {
+                    // send as jsonData
+                    request.jsonData = request.jsonData || {};
+                    if (root) {
+                        request.jsonData[root] = data;
+                    } else {
+                        request.jsonData = data;
+                    }
+                }
+                return request;
+            }
+        });
+    }
 });
